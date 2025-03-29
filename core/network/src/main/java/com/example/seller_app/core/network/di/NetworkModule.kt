65114f898bec6668e526de2173109e50152427ed
KahiloMassango <kahilo42@gmail.com>
2025-03-29 21:01:48 +0100

    chore: change base url

--- core/network/src/main/java/com/example/seller_app/core/network/di/NetworkModule.kt
+++ core/network/src/main/java/com/example/seller_app/core/network/di/NetworkModule.kt
@@ -12,8 +12,8 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-//const val BASE_URL = "http://10.0.2.2:8080/v1/"
-const val BASE_URL = "https://silkworm-immortal-correctly.ngrok-free.app/v1/"
+const val BASE_URL = "http://10.0.2.2:8080/v1/"
+//const val BASE_URL = "https://silkworm-immortal-correctly.ngrok-free.app/v1/"
 
 @Module
 @InstallIn(SingletonComponent::class)
